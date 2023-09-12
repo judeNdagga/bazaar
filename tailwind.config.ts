036dda4f712f9d51dd3dff33f1a144ab4e94c3a1
judeNdagga <ndaggajude88@gmail.com>
2023-09-12 10:22:32 +0300

    first commit

---
+++ tailwind.config.ts
@@ -0,0 +1,41 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#15238e",
+        
+"secondary": "#ef8def",
+        
+"accent": "#f276d1",
+        
+"neutral": "#29242d",
+        
+"base-100": "#f4f2f8",
+        
+"info": "#3fafd5",
+        
+"success": "#66ebcc",
+        
+"warning": "#d0b116",
+        
+"error": "#e8173a",
+
+"body": {
+  "background-color": "#f4f2f8",
+},
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
+export default config
