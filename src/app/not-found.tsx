5a98f3341715ca76a48df020afb16ea1783db9b4
judeNdagga <ndaggajude88@gmail.com>
2023-09-19 17:26:08 +0300

    fix not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,3 @@
-export default function NotFoundPage(){
-    return(
-        <div>Page Not found lol</div>
-    )
+export default function NotFoundPage() {
+  return <div>Page Not found lol</div>;
 }
